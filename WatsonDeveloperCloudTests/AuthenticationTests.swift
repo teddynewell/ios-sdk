5a25d45f874125b5857b6d050f55b2db12a791a6
Glenn R. Fisher <grfisher@us.ibm.com>
2016-03-06 20:07:56 -0500

    Remove XCGLogger
    This logging library is no longer necessary--especially since we do not want to litter our application developers' consoles with internal logging messages.

--- WatsonDeveloperCloudTests/AuthenticationTests.swift
+++ WatsonDeveloperCloudTests/AuthenticationTests.swift
@@ -72,8 +72,6 @@ class AuthenticationTests: XCTestCase {
                 XCTAssertNotNil(fbAuthentication.token)
                 XCTAssertNil(error)
             
-                Log.sharedLogger.info("Received a Watson token \(fbAuthentication.token)")
-            
                 facebookExpectation.fulfill()
             
         }
@@ -119,8 +117,6 @@ class AuthenticationTests: XCTestCase {
             
             XCTAssertNil(fbAuthentication.token)
             XCTAssertNotNil(error)
-            
-            Log.sharedLogger.info("Received a Watson token \(fbAuthentication.token)")
             
             facebookNegativeExpectation.fulfill()
             
